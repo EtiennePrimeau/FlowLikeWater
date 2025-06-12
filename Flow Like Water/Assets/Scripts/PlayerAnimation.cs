6ba6fbcf08b318ebcd4386baea4e99aabfc97ac3
Etienne <etienne412@hotmail.com>
2025-06-11 21:43:34 -0400

    PlayerVisuals & animations

---
+++ Flow Like Water/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    [Header("Animation Controller")]
+    public Animator animator;
+
+    public bool isFront;
+    
+    [Header("Animation Parameters")]
+    private readonly string TRIGGER_LEFT = "TriggerLeft";
+    private readonly string TRIGGER_RIGHT = "TriggerRight";
+    
+    
+    
+    void Start()
+    {
+        if (animator == null)
+            animator = GetComponent<Animator>();
+
+        if (isFront)
+            animator.speed = 0.9f;
+    }
+    
+    public void TriggerLeftPaddle()
+    {
+        if (animator != null)
+            animator.SetTrigger(TRIGGER_LEFT);
+    }
+    
+    public void TriggerRightPaddle()
+    {
+        if (animator != null)
+            animator.SetTrigger(TRIGGER_RIGHT);
+    }
+    
+    // Optional: Get current animation state
+    public bool IsInIdle()
+    {
+        return animator.GetCurrentAnimatorStateInfo(0).IsName("Idle");
+    }
+    
+    public bool IsInLeftPaddle()
+    {
+        return animator.GetCurrentAnimatorStateInfo(0).IsName("PaddleLeft");
+    }
+    
+    public bool IsInRightPaddle()
+    {
+        return animator.GetCurrentAnimatorStateInfo(0).IsName("PaddleRight");
+    }
+}
